ee35436571fee523a2b6bb43aff6e743cecd7f64
apnacollege-demo <sumitydv1910@gmail.com>
2025-08-06 00:27:16 +0530

    first commit

---
+++ Loops/04solution.py
@@ -0,0 +1,10 @@
+Fruit="Banana"
+Color="Yellow"
+if Fruit=="Banana":
+    if color=="Green":
+    Print("unripe")
+    elifcolor =="Yellow"
+    print("Ripe")
+    color=="Brown"
+    print("OverRipe")
+              
